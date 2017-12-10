ab51d4553d7606d4fe05b1ba481219f1ff72a09a
Michal Cermak <Michal Cermak>
2017-12-10 13:57:42 +0100

    Add project files.

---
+++ HL7.FHIR.Code.QuestionnaireResponse/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+
+namespace HL7.FHIR.Code.QuestionnaireResponse
+{
+    static class Program
+    {
+        static void Main()
+        {
+
+            var gPerson = Guid.NewGuid();
+            var gHelseperson = Guid.NewGuid();
+
+            var qResponse = new Hl7.Fhir.Model.QuestionnaireResponse
+            {
+                Id = "1",
+                Meta = new Meta
+                {
+                    Tag = new List<Coding>()
+                    {
+                        new Coding()
+                        {
+                            System = "https://sarepta.ehelse.no/FHIR/QuestionnaireResponse_doedsmelding",
+                            Version = "1.0",
+                        },
+                    },
+                    Profile = new List<string>
+                    {
+                        "https://sarepta.ehelse.no/FHIR/StructureDefinition/SDF-QuestionnaireResponse_RapportDeceasedPerson"
+                    }
+                },
+                Questionnaire = new ResourceReference()
+                {
+                    Url = new Uri("http://nde-fhir-ehelse.azurewebsites.net/fhir/Questionnaire/7")
+                },
+                Authored = DateTime.Now.ToShortDateString(),
+                Status = Hl7.Fhir.Model.QuestionnaireResponse.QuestionnaireResponseStatus.Completed,
+                Subject = new ResourceReference()
+                {
+                    Reference = $"https://grunndata.ehelse.no/fhir/Person/{gPerson}"  
+                },
+                Source = new ResourceReference
+                {
+                    Reference = $"https://grunndata.ehelse.no/fhir/Practitioner/{gHelseperson}"
+                },
+                Item = new List<Hl7.Fhir.Model.QuestionnaireResponse.ItemComponent>
+                {
+                    new Hl7.Fhir.Model.QuestionnaireResponse.ItemComponent
+                    {
+                      LinkId  = "deceasedPerson",
+                      Answer = new List<Hl7.Fhir.Model.QuestionnaireResponse.AnswerComponent>
+                      {
+                          new Hl7.Fhir.Model.QuestionnaireResponse.AnswerComponent
+                          {
+                              //Value = new FhirUri("https://grunndata.ehelse.no/fhir/Person?identitifier=PID|12345678901")
+                              Value = new FhirString("01041843345")
+                          }
+                      }
+                    },
+                    new Hl7.Fhir.Model.QuestionnaireResponse.ItemComponent
+                    {
+                        LinkId = "timeOfDeath",
+                        Answer = new List<Hl7.Fhir.Model.QuestionnaireResponse.AnswerComponent>
+                        {
+                            new Hl7.Fhir.Model.QuestionnaireResponse.AnswerComponent
+                            {
+                                Value = new FhirString(DateTime.Today.AddDays(-1).ToShortDateString())
+                            }
+                        } 
+                        
+                    },
+                    new Hl7.Fhir.Model.QuestionnaireResponse.ItemComponent
+                    {
+                        LinkId = "confirmation",
+                        Answer = new List<Hl7.Fhir.Model.QuestionnaireResponse.AnswerComponent>
+                        {
+                            new Hl7.Fhir.Model.QuestionnaireResponse.AnswerComponent
+                            {
+                                Value = new FhirBoolean(true)
+                            }
+                        }
+                    }
+                    
+                }
+            };
+
+            const string fhirServer = "http://nde-fhir-ehelse.azurewebsites.net/fhir";
+
+            var client = new FhirClient(fhirServer);
+
+            try
+            {
+                var resCreate = client.Update(qResponse);
+                Console.WriteLine($"QuestionnaireResponse ID: {resCreate.Id}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Finished...");
+        }
+    }
+}
---
+++ HL7.FHIR.Core.Questionaire/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+
+namespace HL7.FHIR.Core.Questionaire
+{
+    static class Program
+    {
+        static void Main()
+        {
+
+            var qDødsmelding = new Questionnaire
+            {
+                Id = "7",
+                Title = "Dødsmelding",
+                Name = "doedsmelding_nb_01",
+                Description = new Markdown("Melding som rapporterer dødsfall til Folkeregisteret"),
+                Purpose = new Markdown("Melding som skal fylles ut av lege ved dødsfall"),
+                Publisher = "Direktoratet for e-helse",
+                Date = "2017",
+                Status = PublicationStatus.Draft,
+                Version = "1",
+                Url = "http://nde-fhir-ehelse.azurewebsites.net/fhir/Questionnaire/7",
+                Language = "nb-NO",
+                Meta = new Meta()
+                {
+                  Tag  = new List<Coding>()
+                  {
+                      new Coding()
+                      {
+                          System = "https://sarepta.ehelse.no/FHIR/Questionnaire_doedsmelding",
+                          Version = "1.0",
+                      },
+                      new Coding()
+                      {
+                          System = "urn:ietf:bcp:47",
+                          Code = "nb-NO",
+                          Display = "Norsk (bokmål)"
+                      }
+                  },
+                  Profile = new List<string>()
+                  {
+                      "https://sarepta.ehelse.no/FHIR/StructureDefinition/SDF-Questionnaire"
+                  }
+                },
+                SubjectType = new List<ResourceType?>()
+                {
+                    ResourceType.Person
+                },
+                Item = new List<Questionnaire.ItemComponent>()
+                {
+                    new Questionnaire.ItemComponent()
+                    {
+                        LinkId = "deceasedPerson",
+                        Text = "Personidentifikator til den avdøde",
+                        Required = true,
+                        Repeats = false,
+                        Type = Questionnaire.QuestionnaireItemType.Text,
+                        FhirComments = new List<string>() {"PersonPicker"},
+                        
+                    },
+                    new Questionnaire.ItemComponent()
+                    {
+                        LinkId = "timeOfDeath",
+                        Text = "Dødstidspunkt (dd.mm.åååå)",
+                        Required = true,
+                        Repeats = false,
+                        Type = Questionnaire.QuestionnaireItemType.DateTime
+                    },
+                    new Questionnaire.ItemComponent()
+                    {
+                        LinkId = "confirmation",
+                        EnableWhen = new List<Questionnaire.EnableWhenComponent>()
+                        {
+                            new Questionnaire.EnableWhenComponent()
+                            {
+                                Question = "timeOfDeath",
+                                HasAnswer = true
+                            }
+                        },
+                        Text = "Bekreftelse",
+                        Required = true,
+                        Repeats = false,
+                        Type = Questionnaire.QuestionnaireItemType.Boolean
+                    }
+                }
+            };
+
+
+            const string fhirServer = "http://nde-fhir-ehelse.azurewebsites.net/fhir";
+            //const string fhirServer = "http://vonk.furore.com";
+            var client = new FhirClient(fhirServer);
+
+            try
+            {
+                //var resCreate = client.Create(qDødsmelding);
+
+                var resCreate = client.Update(qDødsmelding);
+                Console.WriteLine($"Questionnaire ID: {resCreate.Id}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Finished...");
+        }
+    }
+}
